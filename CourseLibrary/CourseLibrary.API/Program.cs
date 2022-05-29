4094e129655396a8ef0a259ec6319363a3aad657
APeev89 <apostol.peev@mail.bg>
2022-05-29 12:52:07 +0300

    Adding a Data Store

--- CourseLibrary/CourseLibrary.API/Program.cs
+++ CourseLibrary/CourseLibrary.API/Program.cs
@@ -1,23 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using CourseLibrary.API.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace CourseLibrary.API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // migrate the database.  Best practice = in Main, using service scope
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<CourseLibraryContext>();
+                    // for demo purposes, delete the database & migrate on startup so 
+                    // we can start with a clean slate
+                    context.Database.EnsureDeleted();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            // run the web app
+            host.Run();
+        }
+
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
