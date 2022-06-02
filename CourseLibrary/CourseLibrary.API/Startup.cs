4f19f0bbc91e19cebcf638f87b7e8c1cdeec5cf5
APeev89 <apostol.peev@mail.bg>
2022-06-02 21:55:45 +0300

    Creating CoursesController, CourseDto and CoursesProfile

--- CourseLibrary/CourseLibrary.API/Startup.cs
+++ CourseLibrary/CourseLibrary.API/Startup.cs
@@ -23,10 +23,9 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setupAction =>
-           {
-               setupAction.ReturnHttpNotAcceptable = true;
-           }).AddXmlDataContractSerializerFormatters();
+            services.AddControllers();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
              
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
