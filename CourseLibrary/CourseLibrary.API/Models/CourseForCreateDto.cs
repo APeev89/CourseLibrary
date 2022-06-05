5c06c0ecb549560a0fcc6392ba835f7b948f498c
APeev89 <apostol.peev@mail.bg>
2022-06-05 14:08:03 +0300

    Creating a Custom Attribute

--- CourseLibrary/CourseLibrary.API/Models/CourseForCreateDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseForCreateDto.cs
@@ -1,11 +1,26 @@
-﻿namespace CourseLibrary.API.Models
-{
-    public class CourseForCreateDto
-    {
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
+namespace CourseLibrary.API.Models
+{
+    [CourseTitleMustBeDifferentFromDescriptionAttribute]
+    public class CourseForCreateDto //: IValidatableObject
+    {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
 
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
